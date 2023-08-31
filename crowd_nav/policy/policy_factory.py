d21738e7036c913477fee599fef747c1d8db3e66
aliw521 <115549911+aliw521@users.noreply.github.com>
2023-08-31 08:43:20 +0800

    Update policy_factory.py

--- crowd_nav/policy/policy_factory.py
+++ crowd_nav/policy/policy_factory.py
@@ -10,4 +10,4 @@ policy_factory['orca'] = ORCA
 policy_factory['none'] = none_policy
 policy_factory['social_force'] = SOCIAL_FORCE
 policy_factory['srnn'] = SRNN
-policy_factory['selfAttn_merge_srnn'] = selfAttn_merge_SRNN
+policy_factory['selfAttn_merge_srnn'] = selfAttn_merge_SRNN      #这段代码可用于实现策略的选择和配置，使得用户能够在运行时选择不同的导航策略，并根据策略名称使用相应的对象进行导航
